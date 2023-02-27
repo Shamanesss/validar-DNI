e27323150e2218919c3b902b1df8bb0c5d50ade8
Shamanesss <mentulbenodorna@gmail.com>
2023-02-27 20:09:40 +0100

    resultado

--- custom.js
+++ custom.js
@@ -1,13 +1,28 @@
-let dni = document.getElementById('dni');
+let input = document.getElementById('input');
 let button=document.getElementById('button');
-button.addEventListener('click',letra);
-function letra () {
-    let cadena="TRWAGMYFPDXBNJZSQVHLCKET";
-    let posicion= dni.value%23;
-    console.log(posicion)
-    letra = cadena.substring(posicion,posicion+1);
-    console.log(letra)
+button.addEventListener('click',validar);
+
+
+function validar () {
+    let dni =input.value
+    let numero = dni.slice(0,8);
+    console.log(numero)
+    let letra = dni.slice(8,9).toLowerCase();
+ 
+    console.log (letra)
+    let cadena=["T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E","T"];
+   
+    if (isNaN(numero)|| !isNaN(letra)){
+         alert ("Introduce un numero de 8 digitos y una letra");
+    }else{
+        let resultado = parseInt(numero) % 23;
+        console.log(resultado);
+        if(cadena[resultado]==letra){
+            alert("El DNI es correcto")
+        }else{
+            alert("la letra es incorrecta, la letra correcta para "+ numero +" es: "+cadena[resultado])
+        }
+
+        }  
     
-
-
-    } 
+} 
